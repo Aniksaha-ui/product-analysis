11f55e1b78da8404ff82a5466d82848344c34d6f
Aniksaha-ui <sahaanik1045@gmail.com>
2022-04-03 23:39:08 +0600

    Navbar component done!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Blog from "./components/Blog/Blog";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/blog" element={<Blog />}></Route>
