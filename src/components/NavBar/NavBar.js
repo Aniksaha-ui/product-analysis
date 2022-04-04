2dbac288e82d93aae86051a9415c048be73f59bf
Aniksaha-ui <sahaanik1045@gmail.com>
2022-04-05 00:42:24 +0600

    Active Link Done!

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,50 +1,55 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import CustomLink from "../CustomLink/CustomLink";
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="container">
-        <Link className="navbar-brand" to="/">
-          <img src="images/logo.png" alt="" />
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/review">
-                Review
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/dashboard">
-                Dashboard
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/blog">
-                Blog
-              </Link>
-            </li>
-          </ul>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <CustomLink
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/"
+                >
+                  Home
+                </CustomLink>
+              </li>
+              <li className="nav-item">
+                <CustomLink className="nav-link" to="/review">
+                  Review
+                </CustomLink>
+              </li>
+              <li className="nav-item">
+                <CustomLink className="nav-link" to="/dashboard">
+                  Dashboard
+                </CustomLink>
+              </li>
+              <li className="nav-item">
+                <CustomLink className="nav-link" to="/blog">
+                  Blog
+                </CustomLink>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      {/* <!-- nav section  --> */}
+    </div>
   );
 };
 
