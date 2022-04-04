ac3f92e33abc25e36295b89497dc8354abbc498d
Aniksaha-ui <sahaanik1045@gmail.com>
2022-04-04 11:23:40 +0600

    review page done

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,20 @@
 import React from "react";
+import useReview from "../../Hooks/useReview";
+import ShowReviews from "../ShowReviews/ShowReviews";
 
 const Review = () => {
+  const [reviews, setReviews] = useReview();
+
   return (
-    <div>
-      <h3>This page is for review</h3>
+    <div className="container mt-5">
+      <h1>Review({reviews.length})</h1>
+      <div class="container mt-5">
+        <div class="row row-cols-1 row-cols-lg-3 g-2 g-lg-3">
+          {reviews.map((review) => (
+            <ShowReviews review={review} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
