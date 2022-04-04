d5248665e7948b2058477bb0f2d01998fe566ea6
Aniksaha-ui <sahaanik1045@gmail.com>
2022-04-05 00:24:23 +0600

    lorem text remove and title changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,17 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="container mt-5">
-      <div className="row d-flex align-items-center justify-content-center">
+      <h3 className="text-primary">BMW X3 Reviews</h3>
+      <div className="row d-flex align-items-center justify-content-center mt-4">
         <div className="col-lg-7 col-md-7 col-12">
           <p className="header-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-            explicabo libero ipsa similique. Expedita doloremque placeat animi
-            cumque aliquam? Sit dolores ut omnis odit delectus sed quaerat
-            labore amet fugit.
+            <b>Market introduction:</b> <br />
+            The X3 facelift was launched in India on 20 January, 2022.
+            <br />
+            <b>Engine and specification:</b> <br /> Powering the BMW X3 facelift
+            is a 2.0-litre, four-cylinder, turbocharged petrol that produces
+            252bhp and 350Nm of torque, paired with an eight-speed automatic
+            transmission.
           </p>
         </div>
         <div className="col-lg-5 col-md-5 col-12">
